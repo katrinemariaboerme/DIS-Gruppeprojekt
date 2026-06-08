1d9c1222b8be0c6a333483631ab51ea7bc22a247
boerme <138621843+katrinemariaboerme@users.noreply.github.com>
2026-06-08 23:02:48 +0200

    Implemented helper function check_id() in db_initialization.py which uses a regex to check if a primary key is valid

--- flask-project/db_initialization.py
+++ flask-project/db_initialization.py
@@ -1,5 +1,5 @@
 from database import db_connection
-import csv
+import csv, re
 
 """ Initializes the databse by calling db_connection()"""
 def init_db():
@@ -59,6 +59,17 @@ def init_db():
     c.close()
     conn.close()
 
+"""Checks if a Content or Credits entity has a valid content_id (primary key)"""
+def check_id(content_id):
+    id = str(content_id).strip()
+    result = re.fullmatch(r't[ms][0-9]{1,7}$', id) # only content_id's staring with tm or ts followed by 1-7 arbitrary numbers are considered valid
+    if result == None:
+        return False
+    else:
+        return True # a match was found so title can be added to our database
+    
+
+"""Populates the empty tables with the actual data from the .csv files"""
 def fill_db():
     conn = db_connection()
     c = conn.cursor()
@@ -71,28 +82,29 @@ def fill_db():
         next(titles) # Skipping the column names so we only get the actual data
         
         for content in titles:
-            c.execute(""" 
-                INSERT INTO Content 
-                (content_id, title, show_type, description, release_year, age_certification, runtime, genres, production_countries,
-                seasons, imdb_id, imdb_score, imdb_votes, tmdb_popularity, tmdb_score)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) 
-                ON CONFLICT DO NOTHING; """, (
-                content[0], # content_id
-                content[1], # title
-                content[2], # show_type
-                content[3] if content[3] != '' else None, # description
-                content[4] if content[4] != '' else None, # release_year
-                content[5] if content[5] != '' else None, # age_certification
-                content[6] if content[6] != '' else None, # runtime
-                content[7] if content[7] != '' else None, # genres
-                content[8] if content[8] != '' else None, # production_countries
-                content[9] if content[9] != '' else None, # seasons
-                content[10] if content[10] != '' else None, # imdb_id
-                content[11] if content[11] != '' else None, # imdb_score
-                content[12] if content[12] != '' else None, # imdb_votes
-                content[13] if content[13] != '' else None, # tmdb_popularity
-                content[14] if content[14] != '' else None, # tmdb_score
-            ))
+            if check_id(content[0]):
+                c.execute(""" 
+                    INSERT INTO Content 
+                    (content_id, title, show_type, description, release_year, age_certification, runtime, genres, production_countries,
+                    seasons, imdb_id, imdb_score, imdb_votes, tmdb_popularity, tmdb_score)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) 
+                    ON CONFLICT DO NOTHING; """, (
+                    content[0], # content_id
+                    content[1], # title
+                    content[2], # show_type
+                    content[3] if content[3] != '' else None, # description
+                    content[4] if content[4] != '' else None, # release_year
+                    content[5] if content[5] != '' else None, # age_certification
+                    content[6] if content[6] != '' else None, # runtime
+                    content[7] if content[7] != '' else None, # genres
+                    content[8] if content[8] != '' else None, # production_countries
+                    content[9] if content[9] != '' else None, # seasons
+                    content[10] if content[10] != '' else None, # imdb_id
+                    content[11] if content[11] != '' else None, # imdb_score
+                    content[12] if content[12] != '' else None, # imdb_votes
+                    content[13] if content[13] != '' else None, # tmdb_popularity
+                    content[14] if content[14] != '' else None, # tmdb_score
+                ))
         conn.commit()
     
     # Same population strategy but now for the actors and directors aka the credits.csv data
@@ -101,17 +113,18 @@ def fill_db():
         next(credits_read) # Skipping the column names so we only get the actual data
         
         for person in credits_read:
-            c.execute(""" 
-                INSERT INTO Credits 
-                (person_id, content_id, name, character_name, role)
-                VALUES (%s, %s, %s, %s, %s) 
-                ON CONFLICT DO NOTHING; """, (
-                person[0], # person_id
-                person[1], # content_id
-                person[2], # name
-                person[3] if person[3] != '' else None, # character
-                person[4] if person[4] != '' else None # role (actor or director)
-            ))
+            if check_id(person[1]):    
+                c.execute(""" 
+                    INSERT INTO Credits 
+                    (person_id, content_id, name, character_name, role)
+                    VALUES (%s, %s, %s, %s, %s) 
+                    ON CONFLICT DO NOTHING; """, (
+                    person[0], # person_id
+                    person[1], # content_id
+                    person[2], # name
+                    person[3] if person[3] != '' else None, # character
+                    person[4] if person[4] != '' else None # role (actor or director)
+                ))
         conn.commit()
         conn.close()
         
